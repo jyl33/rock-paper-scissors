cee4f961ab370de5f5a64b41f96972b753c2954a
jyl33 <27440827+jyl33@users.noreply.github.com>
2023-09-15 14:26:36 -0400

    added html, js and css

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const results = document.querySelector(".results");
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0){
@@ -11,18 +13,60 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
+
     if ((playerSelection == "rock" && computerSelection == "paper") || 
     (playerSelection == "paper" && computerSelection == "scissors") || 
     (playerSelection == "scissors" && computerSelection == "rock")){
-        return ("You lose, " + computerSelection + " beats " + playerSelection)
+        results.innerHTML = ("You lose, " + computerSelection + " beats " + playerSelection);
+        return "lose"
     }
     else if(playerSelection == computerSelection){
-        return ("You tied!")
+        results.innerHTML = ("You tied! You both selected " + playerSelection);
+        return "tie"
     }
     else{
-        return ("You win!, " + playerSelection + " beats " + computerSelection)
+        results.innerHTML = ("You win! " + playerSelection + " beats " + computerSelection);
+        return "win"
     }
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+let elements = document.querySelectorAll(".selection");
+let wins = 0;
+let losses = 0;
+    
+let getSelections = (item) => {
+    const computerSelection = getComputerChoice();
+    let gameResult = playRound(item.target.innerText, computerSelection);
+    console.log(gameResult)    
+    if (gameResult == "win") wins++;
+    else if(gameResult == "lose") losses++;
+
+    results.innerHTML = results.innerHTML + ("<br>Wins: " + wins + ", Losses: " + losses);
+
+    console.log("wins: " + wins);
+    console.log("losses " + losses);
+
+    if(wins == 3) {
+        results.innerHTML = ("You win, good job!");
+        console.log("YOU WIN");
+        wins = 0;
+        losses =0;
+    }
+    else if(losses ==3) {
+        results.innerHTML = ("You lost...");
+        console.log("YOU LOSE");
+        wins = 0;
+        losses = 0;
+    }
+}   
+
+elements.forEach((item) => {
+    item.addEventListener('click', getSelections)
+});   
+
+
+
+
+
